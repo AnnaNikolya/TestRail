7335999314ab488c9317fcad108685630abeb52b
AnnaNikolya <90003547+AnnaNikolya@users.noreply.github.com>
2022-01-08 00:57:23 +0300

    New create Page and Step in first test

--- src/test/java/page/ProjectPage.java
+++ src/test/java/page/ProjectPage.java
@@ -4,8 +4,7 @@ import org.openqa.selenium.WebDriver;
 
 public class ProjectPage extends HomePage {
     private static final String PROJECT_URL = "https://testrailmailinator.testrail.io/index.php?/dashboard";
-    public ProjectPage(WebDriver driver) {
-        super(driver);}
+    public ProjectPage(WebDriver driver) {super(driver);}
     public void openProjectPage() {
         driver.get(PROJECT_URL);
     }
--- src/test/java/tests/TestRailTest.java
+++ src/test/java/tests/TestRailTest.java
@@ -13,5 +13,4 @@ public class TestRailTest {
     )
     public class BookingTest extends AbstractTestNGCucumberTests {
     }
-
 }
