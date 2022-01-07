c262de4082053e05cd64bfab75122ac3ec030111
AnnaNikolya <90003547+AnnaNikolya@users.noreply.github.com>
2022-01-08 00:41:15 +0300

    Create Page and Step in first test

--- src/test/java/steps/TestRailSteps.java
+++ src/test/java/steps/TestRailSteps.java
@@ -1,120 +1,129 @@
 package steps;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import page.HomePage;
+import page.ProjectPage;
+import page.SignUpPage;
+
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 
 
 public class TestRailSteps {
+    public static final String HOME_URL = "https://www.gurock.com/testrail/";// ка сдесь не прописывать а вытянуть с HomePage
+    private static final String PROJECT_URL = "https://testrailmailinator.testrail.io/index.php?/dashboard";
+    private WebDriver driver;
+    private SignUpPage signUpPage;
+    private ProjectPage projectPage;
+    private HomePage homePage;
 
-    @When("website is open {string}")
-    public void websiteIsOpen(String fieldName, String fieldValue) {
-        String name;
-        if(fieldName.equals("Username")) name = "email";
-        else name = "password";
-       $(By.name(name)).sendKeys(fieldValue);
+@Before
+    public void init(){
+        WebDriverManager.chromedriver().setup();
     }
 
-
-    @And("for registration click button Try TestRail for Free")
-    public void forRegistrationClickButtonTryTestRailForFree() {
+    @Given("User opens TestRail home page {string}")
+    public void openPage() {
+        open(HOME_URL);
     }
-
+    @And("click button {string}")
+    public void clickTryTestRailForFree() {
+    $(By.xpath("//*[@id='et_builder_outer_content']/div/div/div[5]/div/div[1]/div[2]")).click();
+    }
+    @And("in the field {string} enter a value {string}")
+    public void inTheFieldEnterAValue(String arg0, String arg1) {
+    $("webAddress").shouldHave(exactText("Some text"));
+        $("firstName").shouldHave(exactText("Some text"));
+        $("lastName").shouldHave(exactText("Some text"));
+        $("email").shouldHave(exactText("Some text"));
+        $("expectedUsers").shouldHave(exactText("Some text"));
+    }
     @And("in the field {string} choose a value {string}")
     public void inTheFieldChooseAValue(String arg0, String arg1) {
-    }
 
-    @Then("choose input with text {string}")
+    }
+    @And("choose input with text {string}")
     public void chooseInputWithText(String arg0) {
+
+    }
+    @When("User clicks button {string}")
+    public void userClicksButtonCreateTestRailTrial() {
+    }
+    @Then("Verify .... и тут описываешь свою проверку")
+    public void verifyИТутОписываешьСвоюПроверку() {
     }
 
-    @And("click button Create TestRail Trial")
-    public void clickButtonCreateTestRailTrial() {
-    }
 
-    @Given("API key {string}")
-    public void apiKey(String arg0) {
-    }
 
-    @When("open way {string}")
-    public void openWay(String arg0) {
-    }
 
-    @And("click on tab {string}")
-    public void clickOnTab(String arg0) {
-    }
-
-    @And("clik on {string}")
-    public void clikOn(String arg0) {
-    }
-
-    @And("input Name {string}")
-    public void inputName(String arg0) {
-    }
-
-    @Then("click {string}")
-    public void click(String arg0) {
-    }
-
-    @And("save key")
-    public void saveKey() {
-    }
 
     @When("open {string}")
     public void open(String arg0) {
     }
-
+    @And("click Add Project")
+    public void clickAddProject() {
+    }
     @And("click button Add Project")
     public void clickButtonAddProject() {
     }
-
     @Then("mistake {string}")
     public void mistake(String arg0) {
     }
 
+
+    @When("open page {string}")
+    public void openPage(String arg0) {
+    }
+    @And("click button an Add Project")
+    public void clickButtonAnAddProject() {
+    }
+    @And("in the field {string} enter a value {string}")
+    public void inTheFieldEnterAValue2(String arg0, String arg1) {
+    }
+    @And("click button name Add Project")
+    public void clickButtonNameAddProject() {
+    }
+
+
     @When("open all projects {string}")
     public void openAllProjects(String arg0) {
     }
-
     @And("click Test Runs in created project")
     public void clickTestRunsInCreatedProject() {
     }
-
+    @And("click Add Test Run")
+    public void clickAddTestRun() {
+    }
     @And("click button Add Test Run")
     public void clickButtonAddTestRun() {
     }
 
+
+    @When("open a {string}")
+    public void openA(String arg0) {
+    }
     @And("choose tab Projects")
     public void chooseTabProjects() {
     }
-
+    @And("click Add Example Project")
+    public void clickAddExampleProject() {
+    }
     @And("click button Add Example Project")
     public void clickButtonAddExampleProject() {
     }
-
     @Then("download chart as an image")
     public void downloadChartAsAnImage() {
     }
-
-    @Then("Return to Dashboard and detete project")
-    public void returnToDashboardAndDeteteProject() {
+    @Then("Return to Dashboard and delete project")
+    public void returnToDashboardAndDeleteProject() {
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
